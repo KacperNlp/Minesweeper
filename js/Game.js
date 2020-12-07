18764c7cd57904bc9faf74c943870bbf27ff6bca
Kacper Nalepa <kacper.nlp@wp.pl>
2020-12-07 23:59:58 +0100

    create main class (Game) and made fade in animation for game

---
+++ js/Game.js
@@ -0,0 +1,32 @@
+class Game{
+    constructor({panel}){
+        //assign game/game panel to property 
+        this.panel = panel;
+    }
+    //init animation and 
+    start(){
+        this.#gamePanelFadeIn();
+        this.#startGame();
+    }
+
+    //when site is loaded, game panel will fade in/apper
+    #gamePanelFadeIn(){
+        const panelFade = gsap.timeline();
+
+        panelFade
+               .set(this.panel, {y:100, opacity:0})
+               .to(this.panel, .2, {y:50, opacity:0})
+               .to(this.panel, .5, {y:0, opacity:1});
+    }
+
+    //start new game
+    #startGame(){
+        console.log('start new game')
+    }
+}
+
+const game = new Game({
+    panel: document.getElementById('game'),
+})
+
+game.start();
